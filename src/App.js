a3c49392ddd5c996dd90bb2d184d4bbefc2cfd9a
StEkeng <stephenekeng15@gmail.com>
2025-11-14 14:43:31 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import Banner from './components/Banner';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
-
+import React, { useState } from "react";
+import Product from "./Product";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <Banner />
-      <About />
-      <Services />
-      <Contact />
-    </div>
+  const [cartCount, setCartCount] = useState(0);
 
+  const products = [
+    { id: 1, name: "Wireless Mouse", price: 25 },
+    { id: 2, name: "Keyboard", price: 40 },
+    { id: 3, name: "USB Flash Drive", price: 15 },
+    { id: 4, name: "Laptop Stand", price: 35 }
+  ];
+
+  const addToCart = () => {
+    setCartCount(cartCount + 1);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>🛍️ Product List</h1>
+        <div className="cart">🛒 Cart: {cartCount}</div>
+      </header>
+
+      <div className="product-list">
+        {products.map((item) => (
+          <Product
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            addToCart={addToCart}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
 export default App;
-      
-  
